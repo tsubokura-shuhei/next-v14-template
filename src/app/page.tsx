68c8066b8a7f68a3347ef26d689d42df0e76663c
tsubokura-shuhei <repeat2639@gmail.com>
2024-10-16 23:01:13 +0900

    file fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
-import styles from './page.module.scss';
+
+import Link from 'next/link'
 
 export default function Home() {
-  return <div className={styles.main}>home</div>;
+
+  return (
+    <div>
+      <main>
+        <h1>Photo Appliation</h1>
+        <div>
+          <Link href="/about">
+            about
+          </Link>
+        </div>
+        <div>
+          <Link href="/photos-ssr">
+            Photos - SSR
+          </Link>
+        </div>
+        <div>
+          <Link href="/photos-client">
+            Photos - client
+          </Link>
+        </div>
+      </main>
+    </div>
+  )
 }
